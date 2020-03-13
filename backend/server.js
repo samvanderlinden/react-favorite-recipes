6a212f1c5e0dd0f1d722da3b754a6013b93d3f16
Sam Vanderlinden <samvanderlinden6@gmail.com>
2020-03-12 21:06:58 -0500

    first commit

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const apiPort = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!!!');
+});
+
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
