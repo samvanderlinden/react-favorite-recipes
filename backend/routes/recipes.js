3b7b34dd6ab177dba2be6844ecae1a5468ee7a8e
Sam Vanderlinden <samvanderlinden6@gmail.com>
2020-03-25 21:15:01 -0500

    install react bootstrap

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 let Recipes = require('../models/recipes.model');
 
-
-
 router.route('/').get((req, res) => {
     Recipes.find()
         .then(recipes => {
@@ -12,8 +10,9 @@ router.route('/').get((req, res) => {
 });
 
 router.route('/add').post((req, res) => {
+    console.log('recipe req object: ', req);
     const recipe = req.body;
-    console.log('toDo', recipe);
+    console.log('new recipe', recipe);
     const newRecipe = new Recipes(recipe);
 
     newRecipe.save()
@@ -23,7 +22,7 @@ router.route('/add').post((req, res) => {
 
 router.route('/:id').delete((req, res) => {
     console.log(req.params.id);
-    console.log(req.params);
+    console.log('req', req);
     Recipes.findByIdAndDelete(req.params.id)
     .then(() => res.json('Recipe Deleted'))
     .catch(err => res.status(400).json('Error:' + err));
