a5ed5a862c1d3604b74a820a839aaa4b48eef16c
Sam Vanderlinden <samvanderlinden6@gmail.com>
2020-03-26 20:15:06 -0500

    edit routes, and add proxy to package.json

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
+
+// Recipes Model
 let Recipes = require('../models/recipes.model');
 
-router.route('/').get((req, res) => {
+// @route   GET /recipes
+// @desc    GET all recipes
+// @access  Public
+router.get('/', (req, res) => {
     Recipes.find()
         .then(recipes => {
             res.json(recipes);
@@ -9,18 +14,23 @@ router.route('/').get((req, res) => {
         .catch(err => res.status(400).json('Error:' + err));
 });
 
-router.route('/add').post((req, res) => {
-    console.log('recipe req object: ', req);
+// @route   POST /recipes/add
+// @desc    CREATE new recipe
+// @access  Public
+router.post('/add',(req, res) => {
     const recipe = req.body;
     console.log('new recipe', recipe);
     const newRecipe = new Recipes(recipe);
 
     newRecipe.save()
-        .then(() => res.json('Recipe Added!!!'))
+        .then((recipe) => res.json(recipe))
         .catch(err => res.status(400).json('Error:' + err));
 });
 
-router.route('/:id').delete((req, res) => {
+// @route   DELETE /recipes/:id
+// @desc    DELETE recipe at given id
+// @access  Public
+router.delete('/:id',(req, res) => {
     console.log(req.params.id);
     console.log('req', req);
     Recipes.findByIdAndDelete(req.params.id)
