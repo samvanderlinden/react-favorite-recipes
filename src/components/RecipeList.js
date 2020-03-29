f6e176375d07aae14a09f0babf27b6fcc155f03e
Sam Vanderlinden <samvanderlinden6@gmail.com>
2020-03-29 16:14:08 -0500

    map over list of recipes to create card instead of a list

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,55 +1,74 @@
 import React, { Component } from 'react';
-import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { Container, ListGroup, ListGroupItem, Button, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { v4 as uuid4 } from 'uuid';
 
 class RecipeList extends Component {
-    state = {
-        recipes: [
-            {
-                id: uuid4(),
-                title: 'Mustard chicken',
-                ingredients: [{ item: 'Chicken'}],
-                summary: 'So good'
-            },
-            {
-                id: uuid4(),
-                title: 'Oatmeal',
-                ingredients: [{ item: 'Oats'}, { item: 'Peanut butter'}],
-                summary: 'Energy meal'
-            }
-        ]
+    constructor(props) {
+        super(props);
+        this.state = {
+            recipes: [
+                {
+                    id: uuid4(),
+                    title: 'Mustard chicken',
+                    ingredients: [{ item: 'Chicken' }],
+                    summary: 'So good'
+                },
+                {
+                    id: uuid4(),
+                    title: 'Oatmeal',
+                    ingredients: [{ item: 'Oats' }, { item: 'Peanut butter' }],
+                    summary: 'Energy meal'
+                }
+            ]
+        }
     }
 
     render() {
-        const [{title, ingredients, summary}] = this.state.recipes;
+        const [{ title, ingredients, summary }] = this.state.recipes;
         console.log(title, ingredients, summary);
-        return(
+        return (
             <div>
-                <Container>
-                    <Button 
-                    color="dark" 
-                    style={{marginBottom: '2rem'}} 
-                    onClick={() => {
-                        const recipe = prompt('Enter Recipe');
-                        if (recipe) {
-                            this.setState(state => ({
-                                recipes: [...state.recipes, {id: uuid4(), title:title, ingredients:ingredients[0].item, summary:summary}]
-                            }))
-                        }
-                    }}
+                {this.state.recipes.map((recipe) => (
+                <Card key={recipe.id}>
+                    <CardImg top width="100%" src="" alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>{recipe.title}</CardTitle>
+                        <ul>
+                        {recipe.ingredients.map((ingredient) => (
+                            <li key={ingredient.item}>{ingredient.item}</li>
+                        ))}
+                        </ul>
+                        <CardText>{recipe.summary}</CardText>
+                        <Button>Button</Button>
+                    </CardBody>
+                </Card>
+                ))}
+                
+                {/* <Container>
+                    <Button
+                        color="dark"
+                        style={{ marginBottom: '2rem' }}
+                        onClick={() => {
+                            const recipe = prompt('Enter Recipe');
+                            if (recipe) {
+                                this.setState(state => ({
+                                    recipes: [...state.recipes, { id: uuid4(), title: title, ingredients: ingredients[0].item, summary: summary }]
+                                }))
+                            }
+                        }}
                     >Add Recipe</Button>
                     <ListGroup>
                         <TransitionGroup className="recipe-list">
                             {this.state.recipes.map((recipe) => (
                                 <CSSTransition key={recipe.id} timeout={500} classNames="fade">
                                     <ListGroupItem>Title: {recipe.title}, {recipe.ingredients.map((item) => `${item.item} `)}</ListGroupItem>
-                                    {/* <ListGroupItem>Ingredients: {recipe.ingredients.map((item) => item.item)}</ListGroupItem> */}
                                 </CSSTransition>
                             ))}
                         </TransitionGroup>
                     </ListGroup>
-                </Container>
+
+                </Container> */}
             </div>
         );
     }
