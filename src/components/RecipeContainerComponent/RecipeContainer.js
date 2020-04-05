747b0f5ee6fcac6cc39c665d595ad0e218e0fb48
Sam Vanderlinden <samvanderlinden6@gmail.com>
2020-04-05 11:27:09 -0500

    move recipes state to RecipeContainer component

--- src/components/RecipeContainerComponent/RecipeContainer.js
+++ src/components/RecipeContainerComponent/RecipeContainer.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import RecipeList from './RecipeListComponent/RecipeList';
+import { v4 as uuid4 } from 'uuid';
 
-const RecipeContiner = (props) => {
-    return (
-        <RecipeList />
-    )
+class RecipeContiner extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { //PUT STATE IN 'RecipeContainer' COMPONENT?
+            recipes: [ //INITIALIZE 'recipes' TO EMPTY ARRAY
+                {
+                    id: uuid4(),
+                    title: 'Mustard chicken',
+                    ingredients: [{ item: 'Chicken' }],
+                    summary: 'So good'
+                },
+                {
+                    id: uuid4(),
+                    title: 'Oatmeal',
+                    ingredients: [{ item: 'Oats' }, { item: 'Peanut butter' }],
+                    summary: 'Energy meal'
+                }
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <RecipeList 
+            recipes = {this.state.recipes}/>
+        )
+    }
+
 }
 
 export default RecipeContiner;
--- src/components/RecipeContainerComponent/RecipeListComponent/RecipeList.js
+++ src/components/RecipeContainerComponent/RecipeListComponent/RecipeList.js
@@ -1,51 +1,27 @@
 import React, { Component } from 'react';
 import { Button, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
-import { v4 as uuid4 } from 'uuid';
 
-class RecipeList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { //PUT STATE IN 'RecipeContainer' COMPONENT?
-            recipes: [ //INITIALIZE 'recipes' TO EMPTY ARRAY
-                {
-                    id: uuid4(),
-                    title: 'Mustard chicken',
-                    ingredients: [{ item: 'Chicken' }],
-                    summary: 'So good'
-                },
-                {
-                    id: uuid4(),
-                    title: 'Oatmeal',
-                    ingredients: [{ item: 'Oats' }, { item: 'Peanut butter' }],
-                    summary: 'Energy meal'
-                }
-            ]
-        }
-    }
-
-    render() {
-        const [{ title, ingredients, summary }] = this.state.recipes;
-        console.log(title, ingredients, summary);
-        return (
-            <div>
-                {this.state.recipes.map((recipe) => (
+const RecipeList = (props) => {
+    return (
+        <div>
+            {props.recipes.map((recipe) => (
                 <Card key={recipe.id}>
                     <CardImg top width="100%" src="" alt="Card image cap" />
                     <CardBody>
                         <CardTitle>{recipe.title}</CardTitle>
                         <ul>
-                        {recipe.ingredients.map((ingredient) => (
-                            <li key={ingredient.item}>{ingredient.item}</li>
-                        ))}
+                            {recipe.ingredients.map((ingredient) => (
+                                <li key={ingredient.item}>{ingredient.item}</li>
+                            ))}
                         </ul>
                         <CardText>{recipe.summary}</CardText>
                         <Button color="danger">Delete</Button>
                     </CardBody>
                 </Card>
-                ))}
-            </div>
-        );
-    }
+            ))}
+        </div>
+    );
+        
 }
 
 export default RecipeList;
